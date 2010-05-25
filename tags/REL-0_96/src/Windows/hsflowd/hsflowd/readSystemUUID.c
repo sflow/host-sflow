046efdb40b8f4aaf56a146c76d34904facd3bb8f
neilmckee <neilmckee@c0bd136a-9412-4574-afac-2923e5188ea6>
2010-05-25 23:07:20 +0000

    tagging release 0.96

---
+++ tags/REL-0_96/src/Windows/hsflowd/hsflowd/readSystemUUID.c
@@ -0,0 +1,61 @@
+#if defined(__cplusplus)
+extern "C" {
+#endif
+
+#include "hsflowd.h"
+#include "readSystemUUID.h"
+
+PVOID getUUIDPtr(uint32_t length,smbiosHeader *smbios){
+	//We're not going to parse the whole SMBIOS table here.  Just a quick and dirty to get the UUID
+	smbiosHeader *pTable;
+	smbiosSystemInformation *si;
+	uchar *scanPtr;
+	pTable = smbios;
+	while(pTable < smbios + length){
+		if(pTable->type == SMBIOS_TABLE_SYSTEM_INFORMATION){
+			si = (smbiosSystemInformation*)pTable;
+			return si->uuid;
+		}
+		else{
+			//tables are terminated with double NULL
+			scanPtr = (uchar*)pTable + pTable->length;
+			while(!(*scanPtr == '\0' && *(scanPtr+1) =='\0')){
+				scanPtr++;
+			}
+			scanPtr+=2;
+			pTable = (smbiosHeader*)scanPtr;
+		}
+	}
+	return NULL;
+}
+
+int readSystemUUID(u_char *uuidbuf){
+	RawSMBIOSData *smbuf;
+	DWORD smbufSize;
+	u_char* uuidPtr;
+	int gotData = NO;
+
+	smbufSize = GetSystemFirmwareTable('RSMB',0,NULL,0);
+	if(smbufSize == 0){
+		MyLog(LOG_ERR,"GetSystemFirmwareTable failed 1st call: %d",GetLastError());
+		return gotData;
+	}
+	smbuf = malloc(smbufSize);
+	if( GetSystemFirmwareTable('RSMB',0,(PVOID)smbuf,smbufSize) < smbufSize){
+		MyLog(LOG_ERR,"GetSystemFirmwareTable failed 2nd call: %d",GetLastError());
+		return gotData;
+	}
+	uuidPtr = NULL;
+	uuidPtr = getUUIDPtr(smbuf->Length,(smbiosHeader*)smbuf->SMBIOSTableData);
+	if(!uuidPtr){
+		MyLog(LOG_ERR,"readSystemUUID: failed");
+		return gotData;
+	}
+	memcpy((void*)uuidbuf,uuidPtr,16);
+	gotData = YES;
+	return gotData;
+}
+
+#if defined(__cplusplus)
+} /* extern "C" */
+#endif
