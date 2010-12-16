1e51e826e8ff9231d77443b3ddd4378d88bc775a
neilmckee <neilmckee@c0bd136a-9412-4574-afac-2923e5188ea6>
2010-12-16 07:02:43 +0000

    instantiate interface samplers and pollers for ulog sampling

--- trunk/src/Linux/readPackets.c
+++ trunk/src/Linux/readPackets.c
@@ -13,6 +13,83 @@ extern "C" {
 
 #ifdef HSF_ULOG
 
+
+  /*_________________-----------------------------------__________________
+    _________________   agentCB_getCounters_interface   __________________
+    -----------------___________________________________------------------
+  */
+
+  void agentCB_getCounters_interface(void *magic, SFLPoller *poller, SFL_COUNTERS_SAMPLE_TYPE *cs)
+  {
+    assert(poller->magic);
+    HSP *sp = (HSP *)poller->magic;
+
+    // device name was copied as userData
+    char *devName = (char *)poller->userData;
+    
+    // make sure the counters are up to the second
+    updateNioCounters(sp);
+
+    for(int i = 0; i < sp->adaptorNIOList.num_adaptors; i++) {
+      HSPAdaptorNIO *adaptor = sp->adaptorNIOList.adaptors[i];
+      if(devName && adaptor && adaptor->deviceName && !strcmp(devName, adaptor->deviceName)) {
+	// generic interface counters
+	SFLCounters_sample_element elem = { 0 };
+	elem.tag = SFLCOUNTERS_GENERIC;
+	elem.counterBlock.generic.ifIndex = poller->dsi.ds_index;
+	elem.counterBlock.generic.ifType = 6; // assume ethernet
+	elem.counterBlock.generic.ifSpeed = 0; // $$$
+	elem.counterBlock.generic.ifDirection = 1; // assume full duplex
+	elem.counterBlock.generic.ifStatus = 3; // means ifAdminStatus==up, ifOperstatus==up
+	elem.counterBlock.generic.ifPromiscuousMode = 0; // $$$
+	elem.counterBlock.generic.ifInOctets = adaptor->nio.bytes_in;
+	elem.counterBlock.generic.ifInUcastPkts = adaptor->nio.pkts_in;
+	elem.counterBlock.generic.ifInMulticastPkts = 0; // $$$
+	elem.counterBlock.generic.ifInBroadcastPkts = 0; // $$$
+	elem.counterBlock.generic.ifInDiscards = adaptor->nio.drops_in;
+	elem.counterBlock.generic.ifInErrors = adaptor->nio.errs_in;
+	elem.counterBlock.generic.ifInUnknownProtos = 0; // $$$
+	elem.counterBlock.generic.ifOutOctets = adaptor->nio.bytes_out;
+	elem.counterBlock.generic.ifOutUcastPkts = adaptor->nio.pkts_out;
+	elem.counterBlock.generic.ifOutMulticastPkts = 0; // $$$
+	elem.counterBlock.generic.ifOutBroadcastPkts = 0; // $$$
+	elem.counterBlock.generic.ifOutDiscards = adaptor->nio.drops_out;
+	elem.counterBlock.generic.ifOutErrors = adaptor->nio.errs_out;
+	SFLADD_ELEMENT(cs, &elem);
+	sfl_poller_writeCountersSample(poller, cs);
+      }
+    }
+  }
+
+  /*_________________---------------------------__________________
+    _________________       getSampler          __________________
+    -----------------___________________________------------------
+  */
+
+  static SFLSampler *getSampler(HSP *sp, char *devName, uint32_t ifIndex)
+  {
+    SFLSampler *sampler = sfl_agent_getSamplerByIfIndex(sp->sFlow->agent, ifIndex);
+    if(sampler == NULL) {
+      SFLDataSource_instance dsi;
+      SFL_DS_SET(dsi, 0, ifIndex, 0); // ds_class,ds_index,ds_instance
+      HSPSFlow *sf = sp->sFlow;
+      // add sampler, and poller too
+      uint32_t samplingRate = sf->sFlowSettings ? sf->sFlowSettings->samplingRate : SFL_DEFAULT_SAMPLING_RATE;
+      uint32_t pollingInterval = sf->sFlowSettings ? sf->sFlowSettings->pollingInterval : SFL_DEFAULT_POLLING_INTERVAL;
+      sampler = sfl_agent_addSampler(sf->agent, &dsi);
+      sfl_sampler_set_sFlowFsPacketSamplingRate(sampler, samplingRate);
+      sfl_sampler_set_sFlowFsReceiver(sampler, HSP_SFLOW_RECEIVER_INDEX);
+      SFLPoller *poller = sfl_agent_addPoller(sf->agent, &dsi, sp, agentCB_getCounters_interface);
+      sfl_poller_set_sFlowCpInterval(poller, pollingInterval);
+      sfl_poller_set_sFlowCpReceiver(poller, HSP_SFLOW_RECEIVER_INDEX);
+      // remember the device name to make the lookups easier later.
+      // Don't want to point directly to the SFLAdaptor or SFLAdaptorNIO object
+      // in case it gets freed at some point.  The device name is enough.
+      poller->userData = (void *)strdup(devName);
+    }
+    return sampler;
+  }
+
   /*_________________---------------------------__________________
     _________________      readPackets          __________________
     -----------------___________________________------------------
@@ -79,60 +156,78 @@ extern "C" {
 		  myLog(LOG_INFO, "%s -> %s (ethertype=0x%04X)", macsrc, macdst, ethtype);
 		}
 	      }
+	      
 		
 	      SFL_FLOW_SAMPLE_TYPE fs = { 0 };
-		
+	
+	      SFLSampler *sampler = NULL;
+
 	      // set the ingress and egress ifIndex numbers.
 	      // Can be "INTERNAL" (0x3FFFFFFF) or "UNKNOWN" (0).
 	      if(pkt->indev_name[0]) {
 		SFLAdaptor *in = adaptorListGet(sp->adaptorList, pkt->indev_name);
-		if(in) fs.input = in->ifIndex;
-		// find or create sampler for in->ifIndex $$$
+		if(in && in->ifIndex) {
+		  fs.input = in->ifIndex;
+		  sampler = getSampler(sp, pkt->indev_name, in->ifIndex);
+		}
 	      }
 	      else {
 		fs.input = SFL_INTERNAL_INTERFACE;
 	      }
 	      if(pkt->outdev_name[0]) {
 		SFLAdaptor *out = adaptorListGet(sp->adaptorList, pkt->outdev_name);
-		if(out) fs.output = out->ifIndex;
-		// find or create sampler for out->ifIndex $$$
+		if(out && out->ifIndex) {
+		  fs.output = out->ifIndex;
+		  sampler = getSampler(sp, pkt->outdev_name, out->ifIndex);
+		}
 	      }
 	      else {
 		fs.output = SFL_INTERNAL_INTERFACE;
 	      }
-	      SFLFlow_sample_element hdrElem = { 0 };
-	      hdrElem.tag = SFLFLOW_HEADER;
-	      uint32_t FCS_bytes = 4;
-	      uint32_t maxHdrLen = sp->sFlow->sampler->sFlowFsMaximumHeaderSize;
-	      hdrElem.flowType.header.frame_length = pkt->data_len + FCS_bytes;
-	      hdrElem.flowType.header.stripped = FCS_bytes;
-		
-	      u_char hdr[HSP_MAX_HEADER_BYTES];
-		
-	      if(pkt->mac_len == 14) {
-		// set the header_protocol to ethernet and
-		// reunite the mac header and payload in one buffer
-		hdrElem.flowType.header.header_protocol = SFLHEADER_ETHERNET_ISO8023;
-		memcpy(hdr, pkt->mac, 14);
-		maxHdrLen -= 14;
-		uint32_t payloadBytes = (pkt->data_len < maxHdrLen) ? pkt->data_len : maxHdrLen;
-		memcpy(hdr+14, pkt->payload, payloadBytes);
-		hdrElem.flowType.header.header_length = payloadBytes + 14;
-		hdrElem.flowType.header.header_bytes = hdr;
-		hdrElem.flowType.header.frame_length += 14;
+
+	      if(sampler == NULL) {
+		// maybe ULOG sent us a packet on device lo(?)
+		if(debug > 1) myLog(LOG_INFO, "dropped sample with no ifIndex\n");
 	      }
 	      else {
-		// just point at the payload
-		// $$$ check for IPv6?  (sFlow should really have SFLHEADER_IP as an option here, so
-		// that the collector can decide if it is v4 or v6)
-		hdrElem.flowType.header.header_protocol = SFLHEADER_IPv4;
-		hdrElem.flowType.header.stripped += 14; // assume ethernet
-		hdrElem.flowType.header.header_length = (pkt->data_len < maxHdrLen) ? pkt->data_len : maxHdrLen;
-		hdrElem.flowType.header.header_bytes = pkt->payload;
-	      }
+		SFLFlow_sample_element hdrElem = { 0 };
+		hdrElem.tag = SFLFLOW_HEADER;
+		uint32_t FCS_bytes = 4;
+		uint32_t maxHdrLen = sampler->sFlowFsMaximumHeaderSize;
+		hdrElem.flowType.header.frame_length = pkt->data_len + FCS_bytes;
+		hdrElem.flowType.header.stripped = FCS_bytes;
 		
-	      SFLADD_ELEMENT(&fs, &hdrElem);
-	      sfl_sampler_writeFlowSample(sp->sFlow->sampler, &fs);
+		u_char hdr[HSP_MAX_HEADER_BYTES];
+		
+		if(pkt->mac_len == 14) {
+		  // set the header_protocol to ethernet and
+		  // reunite the mac header and payload in one buffer
+		  hdrElem.flowType.header.header_protocol = SFLHEADER_ETHERNET_ISO8023;
+		  memcpy(hdr, pkt->mac, 14);
+		  maxHdrLen -= 14;
+		  uint32_t payloadBytes = (pkt->data_len < maxHdrLen) ? pkt->data_len : maxHdrLen;
+		  memcpy(hdr+14, pkt->payload, payloadBytes);
+		  hdrElem.flowType.header.header_length = payloadBytes + 14;
+		  hdrElem.flowType.header.header_bytes = hdr;
+		  hdrElem.flowType.header.frame_length += 14;
+		}
+		else {
+		  // no need to copy - just point at the payload
+		  u_char ipversion = pkt->payload[0] >> 4;
+		  if(ipversion != 4 && ipversion != 6) {
+		    if(debug) myLog(LOG_ERR, "received non-IP packet. Encapsulation is unknown");
+		  }
+		  else {
+		    hdrElem.flowType.header.header_protocol = (ipversion == 4) ? SFLHEADER_IPv4 : SFLHEADER_IPv6;
+		    hdrElem.flowType.header.stripped += 14; // assume ethernet was (or will be) the framing
+		    hdrElem.flowType.header.header_length = (pkt->data_len < maxHdrLen) ? pkt->data_len : maxHdrLen;
+		    hdrElem.flowType.header.header_bytes = pkt->payload;
+		  }
+		}
+		
+		SFLADD_ELEMENT(&fs, &hdrElem);
+		sfl_sampler_writeFlowSample(sampler, &fs);
+	      }
 	    }
 	  } 
 	}
