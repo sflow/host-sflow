45aa920471c304ad0241f9b6f342e9620fb84057
neilmckee <neilmckee@c0bd136a-9412-4574-afac-2923e5188ea6>
2011-02-22 22:39:36 +0000

    tagging release 1_12

---
+++ tags/REL-1_12/src/Windows/hsflowd/hsflowd/loadAverage.c
@@ -0,0 +1,57 @@
+#if defined(__cplusplus)
+extern "C" {
+#endif
+
+#include "hsflowd.h"
+#include "readWindowsCounters.h"
+#include "loadAverage.h"
+
+//globals
+double load_1, load_5, load_15;
+PDH_HQUERY cpu_load_query = NULL;
+
+extern int debug;
+
+int calcLoad(){
+	uint32_t queuelen;
+	double cpuload, load;
+
+    queuelen = (uint32_t)readFormattedCounter("\\System\\Processor Queue Length");
+    cpuload = getCpuLoad();
+    if(queuelen > 2)
+        load = cpuload + queuelen - 2;
+    else
+        load = cpuload;
+    load_1 = load_1 * 0.9200 + load * 0.0800;
+    load_5 = load_5 * 0.9835 + load * 0.0165;
+    load_15 = load_15 * 0.9945 + load * 0.0055;
+	return 0;
+}
+
+double getCpuLoad(){
+	PDH_STATUS Status;
+    PDH_HCOUNTER Counter;
+	DWORD dwType;
+	PDH_FMT_COUNTERVALUE Value;
+	double ret = 0;
+	
+	if(!cpu_load_query){
+		Status = PdhOpenQuery(NULL, 0, &cpu_load_query);
+	}
+
+    Status = PdhAddCounter(cpu_load_query, "\\Processor(_Total)\\% Processor Time", 0, &Counter);
+	Status = PdhCollectQueryData(cpu_load_query);
+	Status = PdhGetFormattedCounterValue(Counter, PDH_FMT_DOUBLE, &dwType, &Value);
+	ret = Value.doubleValue * getCpuNum();
+	return ret/100.0;
+}
+
+int getCpuNum(){
+	SYSTEM_INFO si;
+	GetSystemInfo(&si);
+	return (int)si.dwNumberOfProcessors;
+}
+
+#if defined(__cplusplus)
+} /* extern "C" */
+#endif
