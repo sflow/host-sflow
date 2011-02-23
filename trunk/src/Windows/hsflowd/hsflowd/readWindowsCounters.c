b68a5edab7c6fd042e67ee653c7b701f5f59ccd3
robert-jordan <robert-jordan@c0bd136a-9412-4574-afac-2923e5188ea6>
2011-02-23 19:19:26 +0000

    Enabled languages other than English to function correctly.

--- trunk/src/Windows/hsflowd/hsflowd/readWindowsCounters.c
+++ trunk/src/Windows/hsflowd/hsflowd/readWindowsCounters.c
@@ -16,6 +16,10 @@ uint64_t readSingleCounter(char* path)
 	DWORD dwType;
 	PDH_RAW_COUNTER Value;
 	LONGLONG ret = 0;
+	CHAR localizedPath[PDH_MAX_COUNTER_PATH];
+
+	strcpy(localizedPath,path);
+	localizePath(localizedPath);
 
     Status = PdhOpenQuery(NULL, 0, &Query);
     if (Status != ERROR_SUCCESS) 
@@ -23,7 +27,7 @@ uint64_t readSingleCounter(char* path)
         goto Cleanup;
     }
 
-    Status = PdhAddCounter(Query, path, 0, &Counter);
+    Status = PdhAddCounter(Query, localizedPath, 0, &Counter);
     if (Status != ERROR_SUCCESS) 
     {
         goto Cleanup;
@@ -60,6 +64,10 @@ uint32_t readMultiCounter(char* path, PPDH_RAW_COUNTER_ITEM *ppBuffer)
     PDH_HCOUNTER Counter;
 	DWORD bufSize = 0, itemCount = 0;
 	uint32_t ret = 0, i = 0;
+	CHAR localizedPath[PDH_MAX_COUNTER_PATH];
+
+	strcpy(localizedPath,path);
+	localizePath(localizedPath);
 
     Status = PdhOpenQuery(NULL, 0, &Query);
     if (Status != ERROR_SUCCESS) 
@@ -67,7 +75,7 @@ uint32_t readMultiCounter(char* path, PPDH_RAW_COUNTER_ITEM *ppBuffer)
         goto Cleanup;
     }
 
-    Status = PdhAddCounter(Query, path, 0, &Counter);
+    Status = PdhAddCounter(Query, localizedPath, 0, &Counter);
     if (Status != ERROR_SUCCESS) 
     {
         goto Cleanup;
@@ -117,6 +125,10 @@ uint64_t readFormattedCounter(char* path)
 	DWORD dwType;
 	PDH_FMT_COUNTERVALUE Value;
 	LONGLONG ret = 0;
+	CHAR localizedPath[PDH_MAX_COUNTER_PATH];
+
+	strcpy(localizedPath,path);
+	localizePath(localizedPath);
 
     Status = PdhOpenQuery(NULL, 0, &Query);
     if (Status != ERROR_SUCCESS) 
@@ -124,7 +136,7 @@ uint64_t readFormattedCounter(char* path)
         goto Cleanup;
     }
 
-    Status = PdhAddCounter(Query, path, 0, &Counter);
+    Status = PdhAddCounter(Query, localizedPath, 0, &Counter);
     if (Status != ERROR_SUCCESS) 
     {
         goto Cleanup;
@@ -153,6 +165,97 @@ Cleanup:
 
 	return (uint64_t)ret;
 }
+
+uint32_t localizePath(char *path)
+{
+	//See: http://support.microsoft.com/kb/q287159/
+
+	DWORD dwRet = ERROR_SUCCESS;
+	DWORD cbPathData = 0;
+	PDH_COUNTER_PATH_ELEMENTS *pPathElements = NULL;
+	CHAR objectName[PDH_MAX_COUNTER_PATH], counterName[PDH_MAX_COUNTER_PATH];
+	LONG idx = 0;
+
+	dwRet = PdhParseCounterPath(path,pPathElements,&cbPathData,0);
+	if(dwRet == PDH_MORE_DATA){
+		pPathElements = (PDH_COUNTER_PATH_ELEMENTS*)malloc(cbPathData);
+		if(!pPathElements){
+			goto Cleanup;
+		}
+		dwRet = PdhParseCounterPath(path,pPathElements,&cbPathData,0);
+		if(dwRet != PDH_CSTATUS_VALID_DATA){
+			goto Cleanup;
+		}
+	}else{
+		goto Cleanup;
+	}
+
+	idx = getPdhIndex(pPathElements->szObjectName);
+	cbPathData = PDH_MAX_COUNTER_PATH;
+	PdhLookupPerfNameByIndex(NULL,idx,objectName,&cbPathData);
+	pPathElements->szObjectName = objectName;
+
+	idx = getPdhIndex(pPathElements->szCounterName);
+	cbPathData = PDH_MAX_COUNTER_PATH;
+	PdhLookupPerfNameByIndex(NULL,idx,counterName,&cbPathData);
+	pPathElements->szCounterName = counterName;
+
+	cbPathData = PDH_MAX_COUNTER_PATH;
+	PdhMakeCounterPath(pPathElements,path,&cbPathData,0);
+
+Cleanup:
+	if(pPathElements)free(pPathElements);
+
+	return ERROR_SUCCESS;
+}
+
+uint32_t getPdhIndex(LPCSTR oName){
+	LPBYTE next, data;
+	DWORD cbRegData = 1, dwRet = ERROR_SUCCESS;
+	LONG idx = 0;
+
+	//fetch english object and counter names and indexes
+	data = (LPBYTE)malloc(cbRegData);
+	if(!data){
+		goto Cleanup;
+	}
+	dwRet = RegQueryValueEx( HKEY_PERFORMANCE_DATA,
+		                     "Counter 009",
+		                     NULL,
+							 NULL,
+		                     (LPBYTE)data,
+		                     &cbRegData );
+	if(dwRet == ERROR_MORE_DATA){
+		free(data);
+		data = (LPBYTE)malloc(cbRegData);
+		if(!data){
+			goto Cleanup;
+		}
+		dwRet = RegQueryValueEx( HKEY_PERFORMANCE_DATA,
+		                     "Counter 009",
+		                     NULL,
+							 NULL,
+		                     (LPBYTE)data,
+		                     &cbRegData );
+		if(dwRet != ERROR_SUCCESS)
+			goto Cleanup;
+	}
+
+	next = data;
+	while(next < data + cbRegData){
+		if(strcmp(oName,next)==0){
+			break;
+		}
+		idx = atol(next);
+		next +=strlen(next) + 1;
+	}
+
+Cleanup:
+	if(data)free(data);
+
+	return idx;
+}
+
 
 #if defined(__cplusplus)
 } /* extern "C" */
