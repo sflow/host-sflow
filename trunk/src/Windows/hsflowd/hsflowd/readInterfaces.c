ee84eeb25760cfb574e4e3a9fa1a0b194ecedd85
neilmckee <neilmckee@c0bd136a-9412-4574-afac-2923e5188ea6>
2011-08-18 21:01:31 +0000

    implement faster polling for NIO counters in case they turn out to be 32-bit

--- trunk/src/Windows/hsflowd/hsflowd/readInterfaces.c
+++ trunk/src/Windows/hsflowd/hsflowd/readInterfaces.c
@@ -7,120 +7,72 @@ extern "C" {
 
 #include "hsflowd.h"
 
-/*________________---------------------------__________________
-  ________________    freeAdaptors           __________________
-  ----------------___________________________------------------
-*/
+	/*________________---------------------------__________________
+	  ________________      cleanNameForWMI      __________________
+	  ----------------___________________________------------------
+	*/
 
-
-void freeAdaptors(HSP *sp)
-{
-  uint32_t i;
-
-  if(sp->adaptorList) {
-	  if(sp->adaptorList->adaptors) {
-    for( i = 0; i < sp->adaptorList->num_adaptors; i++) {
-      if(sp->adaptorList->adaptors[i]) my_free(sp->adaptorList->adaptors[i]);
-    }
-	my_free(sp->adaptorList->adaptors);
-	  }
-    if(sp->adaptorList) {
-	  my_free(sp->adaptorList);
-	  sp->adaptorList = NULL;
-	}
-  }
-}
-
-  
-/*________________---------------------------__________________
-  ________________    newAdaptorList         __________________
-  ----------------___________________________------------------
-*/
-
-void newAdaptorList(HSP *sp)
-{
-  freeAdaptors(sp);
-  sp->adaptorList = (SFLAdaptorList *)my_calloc(sizeof(SFLAdaptorList));
-  sp->adaptorList->capacity = 4; // will grow if necessary
-  sp->adaptorList->adaptors = (SFLAdaptor **)my_calloc(sp->adaptorList->capacity * sizeof(SFLAdaptor *));
-  sp->adaptorList->num_adaptors = 0;
-}
-
-/*________________---------------------------__________________
-  ________________    trimWhitespace         __________________
-  ----------------___________________________------------------
-*/
-
-static char *trimWhitespace(char *str)
-{
-  char *end;
-  
-  // Trim leading space
-  while(isspace(*str)) str++;
-  
-  // Trim trailing space
-  end = str + strlen(str) - 1;
-  while(end > str && isspace(*end)) end--;
-  
-  // Write new null terminator
-  *(end+1) = 0;
-  
-  return str;
-}
-
-/*________________---------------------------__________________
-  ________________      readInterfaces       __________________
-  ----------------___________________________------------------
-*/
-
-int readInterfaces(HSP *sp)
-{
-  PIP_ADAPTER_INFO pAdapterInfo;
-  PIP_ADAPTER_INFO pAdapter = NULL;
-  DWORD dwRetVal = 0;
-  SFLAdaptor *adaptor;
-  ULONG ulOutBufLen = sizeof (IP_ADAPTER_INFO);
-
-  newAdaptorList(sp);
-
-  pAdapterInfo = (IP_ADAPTER_INFO *) my_calloc(sizeof (IP_ADAPTER_INFO));
-  if (pAdapterInfo == NULL) {
-      myLog(LOG_ERR,"Error allocating memory needed to call GetAdaptersinfo\n");
-      return 1;
-  }
-  if (GetAdaptersInfo(pAdapterInfo, &ulOutBufLen) == ERROR_BUFFER_OVERFLOW) {
-        my_free(pAdapterInfo);
-        pAdapterInfo = (IP_ADAPTER_INFO *) my_calloc(ulOutBufLen);
-        if (pAdapterInfo == NULL) {
-            myLog(LOG_ERR,"Error allocating memory needed to call GetAdaptersinfo\n");
-            return 1;
-        }
-   }
-  if ((dwRetVal = GetAdaptersInfo(pAdapterInfo, &ulOutBufLen)) == NO_ERROR) {
-        pAdapter = pAdapterInfo;
-		while (pAdapter) {
-			adaptor = (SFLAdaptor *)my_calloc(sizeof(SFLAdaptor) + (1 * sizeof(SFLMacAddress)));
-			memcpy(adaptor->macs[0].mac,pAdapter->Address,6);
-			adaptor->num_macs = 1;
-			adaptor->deviceName = my_strdup(pAdapter->AdapterName);
-			adaptor->ifIndex = pAdapter->Index;
-			adaptor->ipAddr.addr = inet_addr(pAdapter->IpAddressList.IpAddress.String);
-			sp->adaptorList->adaptors[sp->adaptorList->num_adaptors] = adaptor;
-			if(++sp->adaptorList->num_adaptors == sp->adaptorList->capacity)  {
-		  	// grow
-		  		sp->adaptorList->capacity *= 2;
-		  		sp->adaptorList->adaptors = (SFLAdaptor **)my_realloc(sp->adaptorList->adaptors,
-								     sp->adaptorList->capacity * sizeof(SFLAdaptor *));
+	void cleanNameForWMI(char *aname) {
+		// fix the adaptor name for WMI counter-name compatibility
+		// TODO: Find list of reserved chars for WMI.
+		for(int i = my_strlen(aname); --i >= 0; ) {
+			switch(aname[i]) {
+			case '/': aname[i] = '_'; break;
+			case '\\': aname[i] = '_'; break;
+			case '(': aname[i] = '['; break;
+			case ')': aname[i] = ']'; break;
+			default: break;
 			}
-			myLog(LOG_INFO,"AdapterInfo:\n\tAdapterName:\t%s\n\tDescription:\t%s\n",pAdapter->AdapterName,pAdapter->Description);
-			pAdapter = pAdapter->Next;
 		}
-  }
-  
+	}
 
-  if (pAdapterInfo) my_free(pAdapterInfo);
-  return sp->adaptorList->num_adaptors;
-}
+	/*________________---------------------------__________________
+ 	  ________________      readInterfaces       __________________
+	  ----------------___________________________------------------
+	*/
+
+	int readInterfaces(HSP *sp)
+	{
+		PIP_ADAPTER_INFO pAdapterInfo;
+		PIP_ADAPTER_INFO pAdapter = NULL;
+		DWORD dwRetVal = 0;
+		ULONG ulOutBufLen = sizeof (IP_ADAPTER_INFO);
+
+		if(sp->adaptorList == NULL) sp->adaptorList = adaptorListNew();
+		else adaptorListMarkAll(sp->adaptorList);
+
+		pAdapterInfo = (IP_ADAPTER_INFO *) my_calloc(sizeof (IP_ADAPTER_INFO));
+		if (pAdapterInfo == NULL) {
+			myLog(LOG_ERR,"Error allocating memory needed to call GetAdaptersinfo\n");
+			return 1;
+		}
+		if (GetAdaptersInfo(pAdapterInfo, &ulOutBufLen) == ERROR_BUFFER_OVERFLOW) {
+			my_free(pAdapterInfo);
+			pAdapterInfo = (IP_ADAPTER_INFO *) my_calloc(ulOutBufLen);
+			if (pAdapterInfo == NULL) {
+				myLog(LOG_ERR,"Error allocating memory needed to call GetAdaptersinfo\n");
+				return 1;
+			}
+		}
+		if ((dwRetVal = GetAdaptersInfo(pAdapterInfo, &ulOutBufLen)) == NO_ERROR) {
+			pAdapter = pAdapterInfo;
+			while (pAdapter) {
+				char *aname = my_strdup(pAdapter->Description);
+				cleanNameForWMI(aname);
+				SFLAdaptor *adaptor = adaptorListAdd(sp->adaptorList, aname, pAdapter->Address, sizeof(HSPAdaptorNIO));
+				// clear the mark so we don't free it below
+				adaptor->marked = NO;
+				adaptor->ifIndex = pAdapter->Index;
+				adaptor->ipAddr.addr = inet_addr(pAdapter->IpAddressList.IpAddress.String);
+				myLog(LOG_INFO,"AdapterInfo:\n\tAdapterName:\t%s\n\tDescription:\t%s\n\tWMI_deviceId:\t<%s>\n",pAdapter->AdapterName, pAdapter->Description, aname);
+				my_free(aname); 
+				pAdapter = pAdapter->Next;
+			}
+		}
+
+		adaptorListFreeMarked(sp->adaptorList);
+		return sp->adaptorList->num_adaptors;
+	}
 
 #if defined(__cplusplus)
 } /* extern "C" */
