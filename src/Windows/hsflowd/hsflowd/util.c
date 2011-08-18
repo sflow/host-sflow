a563b1bff344bcd1f5b90b3deae0d33b8dec80cb
neilmckee <sflow@users.noreply.github.com>
2011-08-18 21:14:38 +0000

    implement faster polling for NIO counters in case they turn out to be 32-bit;  normalize CR/LF line-endings

--- src/Windows/hsflowd/hsflowd/util.c
+++ src/Windows/hsflowd/hsflowd/util.c
@@ -428,12 +428,20 @@ static __declspec(thread) UTHeapRealm utRealm;
     return adList;
   }
 
+  static void adaptorFree(SFLAdaptor *ad)
+  {
+    if(ad) {
+      if(ad->deviceName) my_free(ad->deviceName);
+      if(ad->userData) my_free(ad->userData);
+      my_free(ad);
+    }
+  }
+
   void adaptorListReset(SFLAdaptorList *adList)
   {
     for(uint32_t i = 0; i < adList->num_adaptors; i++) {
       if(adList->adaptors[i]) {
-	my_free(adList->adaptors[i]->deviceName);
-	my_free(adList->adaptors[i]);
+	adaptorFree(adList->adaptors[i]);
 	adList->adaptors[i] = NULL;
       }
     }
@@ -445,6 +453,43 @@ static __declspec(thread) UTHeapRealm utRealm;
     adaptorListReset(adList);
     my_free(adList->adaptors);
     my_free(adList);
+  }
+
+  void adaptorListMarkAll(SFLAdaptorList *adList)
+  {
+    for(uint32_t i = 0; i < adList->num_adaptors; i++) {
+      SFLAdaptor *ad = adList->adaptors[i];
+      if(ad) ad->marked = YES;
+    }
+  }
+
+  void adaptorListFreeMarked(SFLAdaptorList *adList)
+  {
+    uint32_t removed = 0;
+    for(uint32_t i = 0; i < adList->num_adaptors; i++) {
+      SFLAdaptor *ad = adList->adaptors[i];
+      if(ad && ad->marked) {
+	adaptorFree(ad);
+	adList->adaptors[i] = NULL;
+	removed++;
+      }
+    }
+    if(removed > 0) {
+      uint32_t found = 0;
+      // now pack the array and update the num_adaptors count
+      for(uint32_t i = 0; i < adList->num_adaptors; i++) {
+	SFLAdaptor *ad = adList->adaptors[i];
+	if(ad) adList->adaptors[found++] = ad;
+      }
+      // cross-check
+      if((found + removed) != adList->num_adaptors) {
+	myLog(LOG_ERR, "adaptorListFreeMarked: found(%u) + removed(%u) != num_adaptors(%u)",
+	      found,
+	      removed,
+	      adList->num_adaptors);
+      }
+      adList->num_adaptors = found;
+    }
   }
   
   SFLAdaptor *adaptorListGet(SFLAdaptorList *adList, char *dev)
@@ -459,28 +504,219 @@ static __declspec(thread) UTHeapRealm utRealm;
     return NULL;
   }
 
-  SFLAdaptor *adaptorListAdd(SFLAdaptorList *adList, char *dev, u_char *macBytes)
+  SFLAdaptor *adaptorListAdd(SFLAdaptorList *adList, char *dev, u_char *macBytes, size_t userDataSize)
   {
     SFLAdaptor *ad = adaptorListGet(adList, dev);
     if(ad == NULL) {
       ad = (SFLAdaptor *)my_calloc(sizeof(SFLAdaptor));
       ad->deviceName = my_strdup(dev);
-    }
-    if(adList->num_adaptors == adList->capacity) {
-      // grow
-      adList->capacity *= 2;
-      adList->adaptors = (SFLAdaptor **)my_realloc(adList->adaptors, adList->capacity * sizeof(SFLAdaptor *));
-    }
-    adList->adaptors[adList->num_adaptors++] = ad;
-    if(macBytes) {
-      memcpy(ad->macs[0].mac, macBytes, 6);
-      ad->num_macs = 1;
+      ad->userData = my_calloc(userDataSize);
+      
+      if(adList->num_adaptors == adList->capacity) {
+	// grow
+	adList->capacity *= 2;
+	adList->adaptors = (SFLAdaptor **)my_realloc(adList->adaptors, adList->capacity * sizeof(SFLAdaptor *));
+      }
+      adList->adaptors[adList->num_adaptors++] = ad;
+      if(macBytes) {
+	memcpy(ad->macs[0].mac, macBytes, 6);
+	ad->num_macs = 1;
+      }
     }
     return ad;
   }
-    
 
+// #define HSP_USE_GETADAPTERSADDRESSES 
+
+#ifdef HSP_USE_GETADAPTERSADDRESSES
+  /* experimental code to read address information that includes IPv4 and IPv6 addresses */
+
+#include <winsock2.h>
+#include <iphlpapi.h>
+
+/* Link with Iphlpapi.lib */
+#pragma comment(lib, "IPHLPAPI.lib")
+
+#define WORKING_BUFFER_SIZE 15000
+#define MAX_TRIES 3
+
+#define MALLOC(x) my_calloc(x)
+#define FREE(x) my_free(x)
+
+void readAddresses()
+{
+
+    /* Declare and initialize variables */
+
+    DWORD dwSize = 0;
+    DWORD dwRetVal = 0;
+
+    unsigned int i = 0;
+
+    // Set the flags to pass to GetAdaptersAddresses
+    ULONG flags = GAA_FLAG_INCLUDE_PREFIX;
+
+    // default to unspecified address family (both)
+    ULONG family = AF_UNSPEC;
+
+    LPVOID lpMsgBuf = NULL;
+
+    PIP_ADAPTER_ADDRESSES pAddresses = NULL;
+    ULONG outBufLen = 0;
+    ULONG Iterations = 0;
+
+    PIP_ADAPTER_ADDRESSES pCurrAddresses = NULL;
+    PIP_ADAPTER_UNICAST_ADDRESS pUnicast = NULL;
+    PIP_ADAPTER_ANYCAST_ADDRESS pAnycast = NULL;
+    PIP_ADAPTER_MULTICAST_ADDRESS pMulticast = NULL;
+    IP_ADAPTER_DNS_SERVER_ADDRESS *pDnServer = NULL;
+    IP_ADAPTER_PREFIX *pPrefix = NULL;
+
+
+    printf("Calling GetAdaptersAddresses function with family = ");
+    if (family == AF_INET)
+        printf("AF_INET\n");
+    if (family == AF_INET6)
+        printf("AF_INET6\n");
+    if (family == AF_UNSPEC)
+        printf("AF_UNSPEC\n\n");
+
+    // Allocate a 15 KB buffer to start with.
+    outBufLen = WORKING_BUFFER_SIZE;
+
+    do {
+
+        pAddresses = (IP_ADAPTER_ADDRESSES *) MALLOC(outBufLen);
+        if (pAddresses == NULL) {
+            printf
+                ("Memory allocation failed for IP_ADAPTER_ADDRESSES struct\n");
+            exit(1);
+        }
+
+        dwRetVal =
+            GetAdaptersAddresses(family, flags, NULL, pAddresses, &outBufLen);
+
+        if (dwRetVal == ERROR_BUFFER_OVERFLOW) {
+            FREE(pAddresses);
+            pAddresses = NULL;
+        } else {
+            break;
+        }
+
+        Iterations++;
+
+    } while ((dwRetVal == ERROR_BUFFER_OVERFLOW) && (Iterations < MAX_TRIES));
+
+    if (dwRetVal == NO_ERROR) {
+        // If successful, output some information from the data we received
+        pCurrAddresses = pAddresses;
+        while (pCurrAddresses) {
+            printf("\tLength of the IP_ADAPTER_ADDRESS struct: %ld\n",
+                   pCurrAddresses->Length);
+            printf("\tIfIndex (IPv4 interface): %u\n", pCurrAddresses->IfIndex);
+            printf("\tAdapter name: %s\n", pCurrAddresses->AdapterName);
+
+            pUnicast = pCurrAddresses->FirstUnicastAddress;
+            if (pUnicast != NULL) {
+                for (i = 0; pUnicast != NULL; i++)
+                    pUnicast = pUnicast->Next;
+                printf("\tNumber of Unicast Addresses: %d\n", i);
+            } else
+                printf("\tNo Unicast Addresses\n");
+
+            pAnycast = pCurrAddresses->FirstAnycastAddress;
+            if (pAnycast) {
+                for (i = 0; pAnycast != NULL; i++)
+                    pAnycast = pAnycast->Next;
+                printf("\tNumber of Anycast Addresses: %d\n", i);
+            } else
+                printf("\tNo Anycast Addresses\n");
+
+            pMulticast = pCurrAddresses->FirstMulticastAddress;
+            if (pMulticast) {
+                for (i = 0; pMulticast != NULL; i++)
+                    pMulticast = pMulticast->Next;
+                printf("\tNumber of Multicast Addresses: %d\n", i);
+            } else
+                printf("\tNo Multicast Addresses\n");
+
+            pDnServer = pCurrAddresses->FirstDnsServerAddress;
+            if (pDnServer) {
+                for (i = 0; pDnServer != NULL; i++)
+                    pDnServer = pDnServer->Next;
+                printf("\tNumber of DNS Server Addresses: %d\n", i);
+            } else
+                printf("\tNo DNS Server Addresses\n");
+
+            printf("\tDNS Suffix: %wS\n", pCurrAddresses->DnsSuffix);
+            printf("\tDescription: %wS\n", pCurrAddresses->Description);
+            printf("\tFriendly name: %wS\n", pCurrAddresses->FriendlyName);
+
+            if (pCurrAddresses->PhysicalAddressLength != 0) {
+                printf("\tPhysical address: ");
+                for (i = 0; i < (int) pCurrAddresses->PhysicalAddressLength;
+                     i++) {
+                    if (i == (pCurrAddresses->PhysicalAddressLength - 1))
+                        printf("%.2X\n",
+                               (int) pCurrAddresses->PhysicalAddress[i]);
+                    else
+                        printf("%.2X-",
+                               (int) pCurrAddresses->PhysicalAddress[i]);
+                }
+            }
+            printf("\tFlags: %ld\n", pCurrAddresses->Flags);
+            printf("\tMtu: %lu\n", pCurrAddresses->Mtu);
+            printf("\tIfType: %ld\n", pCurrAddresses->IfType);
+            printf("\tOperStatus: %ld\n", pCurrAddresses->OperStatus);
+            printf("\tIpv6IfIndex (IPv6 interface): %u\n",
+                   pCurrAddresses->Ipv6IfIndex);
+            printf("\tZoneIndices (hex): ");
+            for (i = 0; i < 16; i++)
+                printf("%lx ", pCurrAddresses->ZoneIndices[i]);
+            printf("\n");
+
+            printf("\tTransmit link speed: %I64u\n", pCurrAddresses->TransmitLinkSpeed);
+            printf("\tReceive link speed: %I64u\n", pCurrAddresses->ReceiveLinkSpeed);
+
+            pPrefix = pCurrAddresses->FirstPrefix;
+            if (pPrefix) {
+                for (i = 0; pPrefix != NULL; i++)
+                    pPrefix = pPrefix->Next;
+                printf("\tNumber of IP Adapter Prefix entries: %d\n", i);
+            } else
+                printf("\tNumber of IP Adapter Prefix entries: 0\n");
+
+            printf("\n");
+
+            pCurrAddresses = pCurrAddresses->Next;
+        }
+    } else {
+        printf("Call to GetAdaptersAddresses failed with error: %d\n",
+               dwRetVal);
+        if (dwRetVal == ERROR_NO_DATA)
+            printf("\tNo addresses were found for the requested parameters\n");
+        else {
+
+            if (FormatMessage(FORMAT_MESSAGE_ALLOCATE_BUFFER |
+                    FORMAT_MESSAGE_FROM_SYSTEM | FORMAT_MESSAGE_IGNORE_INSERTS, 
+                    NULL, dwRetVal, MAKELANGID(LANG_NEUTRAL, SUBLANG_DEFAULT),   
+                    // Default language
+                    (LPTSTR) & lpMsgBuf, 0, NULL)) {
+                printf("\tError: %s", lpMsgBuf);
+                LocalFree(lpMsgBuf);
+                if (pAddresses)
+                    FREE(pAddresses);
+                exit(1);
+            }
+        }
+    }
+
+    if (pAddresses) {
+        FREE(pAddresses);
+    }
+}
     
+#endif /* HSP_USE_GETADAPTERSADDRESSES */
 #if defined(__cplusplus)
 }  /* extern "C" */
 #endif
