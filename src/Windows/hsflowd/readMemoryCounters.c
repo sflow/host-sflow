f0a9077e8a3a8ba07a43c7e16444e0edf4df2b45
spanchen <spanchen@c0bd136a-9412-4574-afac-2923e5188ea6>
2013-05-02 16:12:18 +0000

    Corrected memory and CPU gauges to use the correct value for unknown (0). Changed memory counters to report free and cached to be consistent with Windows Resource Monitor (which means that memory used can be calculated correctly).

--- src/Windows/hsflowd/readMemoryCounters.c
+++ src/Windows/hsflowd/readMemoryCounters.c
@@ -19,15 +19,13 @@ extern int debug;
  * Returns FALSE if call to GlobalMemoryStatusEx produces an error, TRUE otherwise.
  * Note that the Windows use of memory and classification of use does
  * not map cleanly to Linux terms.
- * We are using Windows available memory for mem_free. Available memory
- * includes standby memory (memory removed from a process's working set 
- * - its physical memory - on route to disk, but is still available to be recalled)
- * and free and zero page list bytes. Windows Resource Monitor reports free memory 
- * as free page list bytes.
- * Memory\\Cache Bytes is used for mem_cached since this really does appear to be
- * the Linux equivalent of cache (file system cache).
  * Windows Resource Monitor reports cached as Standby+Modified this is not the
- * equivalent of Linux file system cache.
+ * equivalent of Linux file system cache, however it could be viewed as
+ * analagous memory usage, and it makes sense to retain some consistency with
+ * Windows tools.
+ * Windows Resource Monitor reports free memory as free and zero page list bytes,
+ * this is used for free memory. There are no obvious equivalents for shared
+ * and buffers so these counters are reported as unknown.
  * Windows also does not seem to report swapping (all memory associated with a process
  * swapped in/out of memory). Memory\\Pages Input/sec and Memory\\Pages Output/sec
  * are used for page_in and page_out.
@@ -41,17 +39,24 @@ BOOL readMemoryCounters(SFLHost_mem_counters *mem)
 		return FALSE;
 	}
 	mem->mem_total = memStat.ullTotalPhys;
-	mem->mem_free = memStat.ullAvailPhys;
 	mem->swap_total = memStat.ullTotalPageFile;
 	mem->swap_free = memStat.ullAvailPageFile;
 	PDH_HQUERY query;
 	if (PdhOpenQuery(NULL, 0, &query) == ERROR_SUCCESS) {
-		PDH_HCOUNTER cache, pageIn, pageOut;
-		if (addCounterToQuery(MEM_COUNTER_OBJECT, NULL, MEM_COUNTER_CACHE, &query, &cache) == ERROR_SUCCESS &&
+		PDH_HCOUNTER free, standbyCore, standbyNormal, standbyReserve, modified, pageIn, pageOut;
+		if (addCounterToQuery(MEM_COUNTER_OBJECT, NULL, MEM_COUNTER_FREE, &query, &free) == ERROR_SUCCESS &&
+			addCounterToQuery(MEM_COUNTER_OBJECT, NULL, MEM_COUNTER_STANDBY_CORE, &query, &standbyCore) == ERROR_SUCCESS &&
+			addCounterToQuery(MEM_COUNTER_OBJECT, NULL, MEM_COUNTER_STANDBY_NORMAL, &query, &standbyNormal) == ERROR_SUCCESS &&
+			addCounterToQuery(MEM_COUNTER_OBJECT, NULL, MEM_COUNTER_STANDBY_RESERVE, &query, &standbyReserve) == ERROR_SUCCESS &&
+			addCounterToQuery(MEM_COUNTER_OBJECT, NULL, MEM_COUNTER_MODIFIED, &query, &modified) == ERROR_SUCCESS &&
 			addCounterToQuery(MEM_COUNTER_OBJECT, NULL, MEM_COUNTER_PAGE_IN, &query, &pageIn) == ERROR_SUCCESS &&
 			addCounterToQuery(MEM_COUNTER_OBJECT, NULL, MEM_COUNTER_PAGE_OUT, &query, &pageOut) == ERROR_SUCCESS &&
 			PdhCollectQueryData(query) == ERROR_SUCCESS) {
-			mem->mem_cached= getRawCounterValue(&cache);
+			mem->mem_free = getRawCounterValue(&free);
+			mem->mem_cached = getRawCounterValue(&standbyCore) +
+				getRawCounterValue(&standbyNormal) +
+				getRawCounterValue(&standbyReserve) +
+				getRawCounterValue(&modified);
 			mem->page_in = (uint32_t)getRawCounterValue(&pageIn);
 			mem->page_out = (uint32_t)getRawCounterValue(&pageOut);
 		}
@@ -59,8 +64,8 @@ BOOL readMemoryCounters(SFLHost_mem_counters *mem)
 	}
 
 	//There are no obvious Windows equivalents
-    mem->mem_shared = UNKNOWN_COUNTER_64;
-	mem->mem_buffers = UNKNOWN_COUNTER_64;
+    mem->mem_shared = UNKNOWN_GAUGE_64;
+	mem->mem_buffers = UNKNOWN_GAUGE_64;
 	//using the definition that swapping is when all the memory associated with a
 	//process is moved in/out of RAM
 	mem->swap_in = UNKNOWN_COUNTER;
@@ -69,8 +74,8 @@ BOOL readMemoryCounters(SFLHost_mem_counters *mem)
 		"readMemoryCounters:\n\ttotal: \t\t%I64d\n\tfree: \t\t%I64d\n"
 		"\tcached: \t%I64d\n\tpage_in: \t%d\n\tpage_out: \t%d\n"
 		"\tswap_total: \t%I64d\n\tswap_free: \t%I64d\n",
-		mem->mem_total,mem->mem_free,
-		mem->mem_cached,mem->page_in,mem->page_out,
+		mem->mem_total, mem->mem_free,
+		mem->mem_cached, mem->page_in, mem->page_out,
 		mem->swap_total, mem->swap_free);
 	return TRUE;
 }
