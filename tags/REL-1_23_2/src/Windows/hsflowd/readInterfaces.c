639f30aa513853579789f5d56ce7fbd15b57a7b8
neilmckee <neilmckee@c0bd136a-9412-4574-afac-2923e5188ea6>
2013-05-10 18:47:16 +0000

    tagging release 1_23_2

---
+++ tags/REL-1_23_2/src/Windows/hsflowd/readInterfaces.c
@@ -0,0 +1,399 @@
+/* This software is distributed under the following license:
+ * http://host-sflow.sourceforge.net/license.html
+ */
+
+#if defined(__cplusplus)
+extern "C" {
+#endif
+
+#include "hsflowd.h"
+#include <Mstcpip.h>
+
+#define VMSMP L"VMSMP"
+#define PROP_GUID L"GUID"
+#define PROP_IFGUID L"InterfaceGuid"
+#define PROP_NAME L"Name"
+#define PROP_IFDESC L"InterfaceDescription"
+#define PROP_MAC L"MACAddress"
+#define PROP_PERMADDR L"PermanentAddress"
+#define PROP_IFINDEX L"InterfaceIndex"
+#define PROP_SVC_NAME L"ServiceName"
+#define PROP_VIRTUAL L"Virtual"
+
+
+ /**
+ * Frees the allocated memory for a HSPAdaptorNIO.
+ */
+void freeAdaptorInfo(void *info)
+{
+	HSPAdaptorNIO *adaptorInfo = (HSPAdaptorNIO *)info;
+	if (info != NULL && adaptorInfo->countersInstance != NULL) {
+		my_free(adaptorInfo->countersInstance);
+	}
+	my_free(adaptorInfo);
+}
+
+/**
+ * Parses addrString into an IPv4 or IPv6 address, then determines
+ * whether the priority of the address is higher than the existing
+ * adaptor IP address (according to EnumIPSelectionPriority).
+ * If it is, then the new address is assigned as the adaptor IP address
+ * and used to populate adapter->userData->ipAddr.
+ * If two addresses have the same highest priority, then the first one seen 
+ * is chosen.
+ */
+static void stringToAdaptorIp(PCWSTR addrString, SFLAdaptor *adaptor)
+{
+	HSPAdaptorNIO *nioState = (HSPAdaptorNIO *)adaptor->userData;
+	IN_ADDR in_addr = {0};
+	LPCWSTR terminator;
+	LONG result = RtlIpv4StringToAddressW(addrString, TRUE, &terminator, &in_addr);
+	if (NO_ERROR == result) {
+		SFLAddress addrv4;
+		addrv4.type = SFLADDRESSTYPE_IP_V4;
+		addrv4.address.ip_v4.addr =  in_addr.S_un.S_addr;
+		EnumIPSelectionPriority ipPriority = agentAddressPriority(&addrv4);
+		if (ipPriority > nioState->ipPriority) {
+			nioState->ipPriority = ipPriority;
+			nioState->ipAddr = addrv4;
+		}
+	} else {
+		IN6_ADDR in6_addr = {0};
+		result = RtlIpv6StringToAddressW(addrString, &terminator, &in6_addr);
+		if (NO_ERROR == result) {
+			SFLAddress addrv6;
+			addrv6.type = SFLADDRESSTYPE_IP_V6;
+			memcpy(addrv6.address.ip_v6.addr, in_addr6.u.Byte, sizeof(in6_addr.u.Byte));
+			EnumIPSelectionPriority ipPriority = agentAddressPriority(&addrv6);
+			if (ipPriority > nioState->ipPriority) {
+				nioState->ipPriority = ipPriority;
+				nioState->ipAddr = addrv6;
+			}
+		}
+	}
+}
+
+/**
+ * Enumerates MSFT_NetIpAddress whose interface index is the same as the
+ * adaptor's. Calls stringToAdaptorIP() for each object so that the
+ * highest priority IP address is associated with the adaptor.
+ */
+static void readIpAddressesMsft(IWbemServices *pNamespace, SFLAdaptor *adaptor)
+{
+	BSTR queryLang = SysAllocString(L"WQL");
+	wchar_t *query = L"SELECT * FROM MSFT_NetIpAddress WHERE InterfaceIndex=%u";
+	wchar_t ipQuery[70];
+	swprintf_s(ipQuery, 70, query, adaptor->ifIndex);
+	IEnumWbemClassObject *ipEnum = NULL;
+	HRESULT hr = pNamespace->ExecQuery(queryLang, ipQuery, WBEM_FLAG_FORWARD_ONLY, NULL, &ipEnum);
+	SysFreeString(queryLang);
+	if (!SUCCEEDED(hr)) {
+		myLog(LOG_ERR,"readInterfacesMsft: ExecQuery() failed for query %S error=0x%x", ipQuery, hr);
+		return;
+	}
+	IWbemClassObject *ipObj;
+	hr = WBEM_S_NO_ERROR;
+	while (WBEM_S_NO_ERROR == hr) {
+		ULONG ipCount = 0;
+		hr = ipEnum->Next(WBEM_INFINITE, 1, &ipObj, &ipCount);
+		if (ipCount == 0) {
+			break;
+		}
+		VARIANT address;
+		hr = ipObj->Get(L"IPAddress", 0, &address, 0, 0);
+		if (WBEM_S_NO_ERROR == hr && V_VT(&address) == VT_BSTR)  {
+			stringToAdaptorIp(address.bstrVal, adaptor);
+		}
+		VariantClear(&address);
+		ipObj->Release();
+	}
+	ipEnum->Release();
+}
+
+/**
+ * Enumerates the adapters from root\standardcimv2 MSFT_NetAdapter,
+ * which is supported from Win8/2012. This method is preferred since
+ * the interface description, which is used to name the counters, 
+ * is not changed when a team is created.
+ * Returns true on success, false on failure.
+ * Uses the information to populate the sp->adaptorList structure:
+ * adapter->deviceName = MSFT_NetAdapter.InterfaceGuid (converted to 
+ * lowercase char with enclosing {} removed)
+ * adapter->ifIndex = MSFT_NetAdapter.InterfaceIndex
+ * adapter->mac[0] = MSFT_NetAdapter.PermanentAddress (this might not
+ * be correct but it is just as correct as chosing the first or random
+ * address from the NetworkAddresses property since the SFLAdapter 
+ * struct only allows one MAC).
+ * this is the interface index used in the route table (rather than Index
+ * which is the index for the interface in the registry).
+ * adapter->userData->countersInstance = MSFT_NetAdapter.InterfaceDescription 
+ * (with reserved chars replaced) 
+ * adapter->userData->isVirtual = MSFT_NetAdapter.Virtual
+ * Optionally gets the IP address (v4 and/or v6) from the associated
+ * MSFT_NetIPAddress filtering on the interfaceIndex. 
+ * This is only required when trying
+ * to identify the IP addresses that could be used as the agent address.
+ */
+BOOL readInterfacesMsft(SFLAdaptorList *adaptorList, BOOL getIpAddr)
+{
+	BSTR path = SysAllocString(WMI_STD_CIMV2_NS);
+	HRESULT hr = S_FALSE;
+	IWbemServices *pNamespace = NULL;
+	
+	hr = connectToWMI(path, &pNamespace);
+	SysFreeString(path);
+	if (WBEM_S_NO_ERROR != hr) {
+		myLog(LOG_INFO,"readInterfacesMsft: connectToWMI failed for namespace %S", path);
+		return FALSE;
+	}
+	BSTR queryLang = SysAllocString(L"WQL");
+	BSTR query = SysAllocString(L"SELECT * FROM MSFT_NetAdapter");
+	IEnumWbemClassObject *adapterEnum = NULL;
+	hr = pNamespace->ExecQuery(queryLang, query, WBEM_FLAG_FORWARD_ONLY, NULL, &adapterEnum);
+	SysFreeString(queryLang);
+		if (!SUCCEEDED(hr)) {
+		myLog(LOG_ERR,"readInterfacesMsft: ExecQuery() failed for query %S error=0x%x", query, hr);
+		SysFreeString(query);
+		pNamespace->Release();
+		CoUninitialize();
+		return FALSE;
+	}
+	SysFreeString(query);
+	IWbemClassObject *adapterObj = NULL;
+	VARIANT ifIndexVal;
+	VARIANT virtualVal;
+	hr = WBEM_S_NO_ERROR;
+	while (WBEM_S_NO_ERROR == hr) {
+		ULONG adapterCount = 1;
+		hr = adapterEnum->Next(WBEM_INFINITE, 1, &adapterObj, &adapterCount);
+		if (0 == adapterCount) {
+			break;
+		}
+		wchar_t *guidString = stringFromWMIProperty(adapterObj, PROP_IFGUID);
+		wchar_t *macString = stringFromWMIProperty(adapterObj, PROP_PERMADDR);
+		if (guidString != NULL && macString != NULL) {
+			u_char deviceName[FORMATTED_GUID_LEN+1];
+			guidToString(guidString, deviceName, FORMATTED_GUID_LEN);
+			u_char mac[13];
+			wchexToBinary(macString, mac, 13);
+			SFLAdaptor *adaptor = adaptorListAdd(adaptorList, 
+												(char *)deviceName, mac, 
+												sizeof(HSPAdaptorNIO));
+			// clear the mark so we don't free it later
+			adaptor->marked = FALSE;
+			if (WBEM_S_NO_ERROR == adapterObj->Get(PROP_IFINDEX, 0, &ifIndexVal, 0, 0) &&
+				(V_VT(&ifIndexVal) == VT_I4 || V_VT(&ifIndexVal) == VT_UI4)) {
+				adaptor->ifIndex = ifIndexVal.ulVal;
+			}
+			HSPAdaptorNIO *userData = (HSPAdaptorNIO *)adaptor->userData;
+			if (userData->countersInstance != NULL) {
+				my_free(userData->countersInstance);
+			}
+			wchar_t *counterName = stringFromWMIProperty(adapterObj, PROP_IFDESC);
+			if (counterName != NULL) {
+				cleanCounterName(counterName, UTNETWORK_INTERFACE);
+				userData->countersInstance = counterName;
+			}
+			if (WBEM_S_NO_ERROR == adapterObj->Get(PROP_VIRTUAL, 0, &virtualVal, 0, 0) &&
+				(V_VT(&virtualVal) == VT_BOOL)) {
+				userData->isVirtual = virtualVal.boolVal != 0; // VARIANT_BOOL is a short
+			}
+			if (getIpAddr) {
+				userData->ipPriority = IPSP_NONE;
+				readIpAddressesMsft(pNamespace, adaptor);
+			}
+			myLog(LOG_INFO,"ReadInterfacesMsft:\n\tAdapterName:\t%s\n\tifIndex:\t%lu\n\tCounterName:\t%S\n\tisVirtual\t%u",
+				adaptor->deviceName, adaptor->ifIndex, userData->countersInstance, userData->isVirtual);
+		}
+		if (guidString != NULL) {
+			my_free(guidString);
+		}
+		if (macString != NULL) {
+			my_free(macString);
+		}
+		adapterObj->Release();
+		VariantClear(&ifIndexVal);
+		VariantClear(&virtualVal);
+	}
+	adapterEnum->Release();
+	pNamespace->Release();
+	CoUninitialize();
+	return TRUE;
+}
+
+ /**
+ * Finds the associated Win32_NetworkAdapterConfiguration for Win32_NetworkAdapter adapterObj.
+ * Iterates through the IP addresses associated with the adapter calling
+ * stringToAdaptorIp() to choose the highest priority address (according to EnumIPSelectionPriority)
+ * as the adapter address which is used to populate adapter->userData->ipAddr.
+ * If two addresses have the same highest priority, then the first one seen is chosen.
+ */
+static void readIpAddressesWin32(IWbemServices *pNamespace, IWbemClassObject *adapterObj, SFLAdaptor *adaptor)
+{
+	IEnumWbemClassObject *configEnum;
+	HRESULT hr = associatorsOf(pNamespace, adapterObj,
+							   L"Win32_NetworkAdapterSetting",
+							   L"Win32_NetworkAdapterConfiguration",
+							   L"Setting", &configEnum);
+	if (SUCCEEDED(hr)) {
+		IWbemClassObject *configObj;
+		ULONG configCount;
+		hr = configEnum->Next(WBEM_INFINITE, 1, &configObj, &configCount);
+		if (SUCCEEDED(hr) && configCount == 1) {
+			VARIANT addresses;
+			hr = configObj->Get(L"IPAddress", 0, &addresses, 0, 0);
+			if (WBEM_S_NO_ERROR == hr && addresses.vt == (VT_ARRAY | VT_BSTR))  {
+				SAFEARRAY *sa = V_ARRAY(&addresses);
+				LONG lstart, lend;
+				hr = SafeArrayGetLBound(sa, 1, &lstart);
+				hr = SafeArrayGetUBound(sa, 1, &lend);
+				BSTR *pbstr;
+				hr = SafeArrayAccessData(sa, (void HUGEP **)&pbstr);
+				if (SUCCEEDED(hr)) {
+					for (LONG idx=lstart; idx <= lend; idx++) {		
+						PCWSTR addrStr = pbstr[idx];
+						stringToAdaptorIp(addrStr, adaptor);
+					}
+					SafeArrayUnaccessData(sa);
+				}
+			}
+			VariantClear(&addresses);
+			configObj->Release();
+		}
+		configEnum->Release();
+	}
+}
+
+/**
+ * Enumerates the adapters for this host from WMI Win32_NetworkAdapter
+ * where NetConnectionStatus=2 (to exclude tunnels, ras, wan miniports etc).
+ * Uses the information to populate the sp->adaptorList structure.
+ * adapter->deviceName = Win32_NetworkAdapter.GUID (converted to 
+ * lowercase char with enclosing {} removed)
+ * adapter->ifIndex = Win32_NetworkAdapter.InterfaceIndex
+ * this is the interface index used in the route table (rather than Index
+ * which is the index for the interface in the registry).
+ * adapter->userData->countersInstance = Win32_NetworkAdapter.Name 
+ * (with reserved chars replaced) 
+ * adapter->userData->isVirtual = (Win32_NetworkAdapter.ServiceName == "VMSMP")
+ * Optionally gets the IP address (v4 and/or v6) from the associated
+ * Win32_NetworkAdapterConfiguration. This is only required when trying
+ * to identify the IP addresses that could be used as the agent address.
+ * Returns true on success, false on failure.
+ */
+static BOOL readInterfacesWin32(SFLAdaptorList *adaptorList, BOOL getIpAddr)
+{
+	BSTR path = SysAllocString(WMI_CIMV2_NS);
+	HRESULT hr = S_FALSE;
+	IWbemServices *pNamespace = NULL;
+	
+	hr = connectToWMI(path, &pNamespace);
+	SysFreeString(path);
+	if (WBEM_S_NO_ERROR != hr) {
+		myLog(LOG_ERR,"readInterfacesWin32: connectToWMI failed for namespace %S", path);
+		return FALSE;
+	}
+	BSTR queryLang = SysAllocString(L"WQL");
+	BSTR query = SysAllocString(L"SELECT * FROM Win32_NetworkAdapter WHERE NetConnectionStatus=2");
+	IEnumWbemClassObject *adapterEnum = NULL;
+	hr = pNamespace->ExecQuery(queryLang, query, WBEM_FLAG_FORWARD_ONLY, NULL, &adapterEnum);
+	SysFreeString(queryLang);
+	if (!SUCCEEDED(hr)) {
+		myLog(LOG_ERR,"readInterfacesWin32: ExecQuery() failed for query %S error=0x%x", query, hr);
+		SysFreeString(query);
+		pNamespace->Release();
+		CoUninitialize();
+		return FALSE;
+	}
+	SysFreeString(query);
+	IWbemClassObject *adapterObj = NULL;
+	VARIANT ifIndexVal;
+	hr = WBEM_S_NO_ERROR;
+	while (WBEM_S_NO_ERROR == hr) {
+		ULONG adapterCount = 1;
+		hr = adapterEnum->Next(WBEM_INFINITE, 1, &adapterObj, &adapterCount);
+		if (0 == adapterCount) {
+			break;
+		}
+		wchar_t *guidString = stringFromWMIProperty(adapterObj, PROP_GUID);
+		wchar_t *macString = stringFromWMIProperty(adapterObj, PROP_MAC);
+		if (guidString != NULL && macString != NULL) {
+			u_char deviceName[FORMATTED_GUID_LEN+1];
+			guidToString(guidString, deviceName, FORMATTED_GUID_LEN);
+			u_char mac[13];
+			wchexToBinary(macString, mac, 13);
+			SFLAdaptor *adaptor = adaptorListAdd(adaptorList, 
+												(char *)deviceName, mac, 
+												sizeof(HSPAdaptorNIO));
+			// clear the mark so we don't free it later
+			adaptor->marked = FALSE;
+			if (WBEM_S_NO_ERROR == adapterObj->Get(PROP_IFINDEX, 0, &ifIndexVal, 0, 0) &&
+				(V_VT(&ifIndexVal) == VT_I4 || V_VT(&ifIndexVal) == VT_UI4)) {
+				adaptor->ifIndex = ifIndexVal.ulVal;
+			}
+			HSPAdaptorNIO *userData = (HSPAdaptorNIO *)adaptor->userData;
+			if (userData->countersInstance != NULL) {
+				my_free(userData->countersInstance);
+			}
+			wchar_t *counterName = stringFromWMIProperty(adapterObj, PROP_NAME);
+			if (counterName != NULL) {
+				cleanCounterName(counterName, UTNETWORK_INTERFACE);
+				userData->countersInstance = counterName;
+			}
+			wchar_t *svcName = stringFromWMIProperty(adapterObj, PROP_SVC_NAME);
+			if (svcName != NULL) {
+				userData->isVirtual = (_wcsicmp(VMSMP, svcName) == 0);
+				my_free(svcName);
+			}
+			if (getIpAddr) {
+				userData->ipPriority = IPSP_NONE;
+				readIpAddressesWin32(pNamespace, adapterObj, adaptor);
+			}
+			myLog(LOG_INFO,"ReadInterfacesWin32:\n\tAdapterName:\t%s\n\tifIndex:\t%lu\n\tCounterName:\t%S\n\tisVirtual\t%u",
+				adaptor->deviceName, adaptor->ifIndex, userData->countersInstance, userData->isVirtual);
+		}
+		if (guidString != NULL) {
+			my_free(guidString);
+		}
+		if (macString != NULL) {
+			my_free(macString);
+		}
+		adapterObj->Release();
+		VariantClear(&ifIndexVal);
+	}
+	adapterEnum->Release();
+	pNamespace->Release();
+	CoUninitialize();
+	return TRUE;
+}
+
+ /**
+ * Enumerates the adapters for this host by first trying to enumerate
+ * MSFT_NetAdapter and if this fails (root\standardcimv2 namespace does
+ * not exist prior to Win8/2012) then tries to enumerate Win32_NetworkAdapter
+ * where NetConnectionStatus=2 (to exclude tunnels, ras, wan miniports etc).
+ * Uses the information to populate the sp->adaptorList structure.
+ * Optionally gets the IP address (v4 and/or v6) from the approriate associated
+ * objects (MSFT_NetIpAddress or Win32_NetworkAdapterConfiguration). 
+ * This is only required when trying to identify the IP addresses that could be 
+ * used as the agent address.
+ * MSFT_NetAdapter is preferred, since the interface description is used for
+ * the interface counter instance name, and this description does not
+ * change when teaming is enabled, whereas Win32_NetworkAdapter.Name does
+ * change when teaming is enabled.
+ */
+ void readInterfaces(HSP *sp, BOOL getIPAddr)
+ {
+	if (sp->adaptorList == NULL) {
+		sp->adaptorList = adaptorListNew();
+	}
+	adaptorListMarkAll(sp->adaptorList);
+	if (!readInterfacesMsft(sp->adaptorList, getIPAddr)) {
+		readInterfacesWin32(sp->adaptorList, getIPAddr);
+	}
+	adaptorListFreeMarked(sp->adaptorList, freeAdaptorInfo);
+ }
+
+#if defined(__cplusplus)
+} /* extern "C" */
+#endif
---
+++ tags/REL-1_23_2/src/Windows/hsflowd/readWindowsEnglishCounters.c
@@ -0,0 +1,182 @@
+/* This software is distributed under the following license:
+ * http://host-sflow.sourceforge.net/license.html
+ */
+
+#if defined(__cplusplus)
+extern "C" {
+#endif
+
+#include "hsflowd.h"
+#include "readWindowsEnglishCounters.h"
+
+extern int debug;
+
+/**
+ * Functions which use the PDH API to obtain counter values.
+ * Uses the pdhAddEnglishCounter function, so can only be used post WinXP,
+ * but means that callers don't need to use the registry to localise the
+ * counter names for non-English locales.
+ * Also uses wchar_t * paths since WMI returns identifiers (for counter instances)
+ * as BSTR (wchar_t *) and since some instances could be set in a non-English locale
+ * cannot assume we can convert to char *.
+ */
+
+/**
+ * Returns the wchar_t * English string path to the specified counter for
+ * use with the PDH API.
+ * Memory is allocated for the counter path using my_calloc, so the caller 
+ * must free with my_free when the path is no longer needed.
+ * Does not convert the counter name to the current locale, assuming
+ * that pdhAddEnglishCounter function is available (post XP).
+ * wchar_t *object performance counter object
+ * (eg Hyper-V Virtual Network Adapter)
+ * wchar_t *instance counter instance (eg switch port name) use 
+ * L"*" for a wild card, NULL for no instance.
+ * wchar_t *counter name of counter.
+ */
+static LPWSTR getCounterPath(wchar_t *object, wchar_t *instance, wchar_t *counter)
+{
+	PDH_COUNTER_PATH_ELEMENTS_W cpe;
+	cpe.szMachineName = NULL;
+	cpe.szObjectName = object;
+	cpe.szInstanceName = instance;
+	cpe.szParentInstance = NULL;
+	cpe.dwInstanceIndex = 0;
+	cpe.szCounterName = counter;
+	LPWSTR szFullPath = NULL;
+	DWORD cbPathSize = 0;
+	PDH_STATUS s;
+
+	s = PdhMakeCounterPathW(&cpe, NULL, &cbPathSize, 0);
+	if (s == PDH_MORE_DATA) {
+		szFullPath = (wchar_t *)my_calloc(cbPathSize*sizeof(wchar_t));
+		s = PdhMakeCounterPathW(&cpe, szFullPath, &cbPathSize, 0);
+		//myLog(LOG_INFO, "PdhMakeCounterPathW: Counter path=%S", szFullPath);
+	}
+	return szFullPath;
+}
+
+/**
+ * Initialises a query to access a single specified counter.
+ * Returns the PDH error code resulting from PdhOpenQuery
+ * and PdhAddEnglishCounterW.
+ * wchar_t *object performance counter object (eg Processor)
+ * wchar_t *instance counter instance (eg _Total) use 
+ * L"*" for a wild card.
+ * wchar_t *counterName English name of counter (eg % Processor Time).
+ * PDH_HQUERY *query handle to the query that will be used in subsequent
+ * calls (eg adding additional counters, running the query - PdhCollectQueryData). 
+ * Set to NULL if initialising the query fails.
+ * PDH_HCOUNTER *counter handle to the counter that will contain the
+ * counter query results and accessed in subsequent calls (eg getRawCounterValue,
+ * PdhGetFormattedCounterValue).
+ */
+PDH_STATUS makeSingleCounterQuery(wchar_t *object, wchar_t *instance, wchar_t *counterName,
+								  PDH_HQUERY *query, PDH_HCOUNTER *counter)
+{
+	PDH_STATUS status = PdhOpenQuery(NULL, 0, query);
+	if (ERROR_SUCCESS == status) {
+		status = addCounterToQuery(object, instance, counterName, query, counter);
+	}
+	if (query && ERROR_SUCCESS != status) {
+		PdhCloseQuery(*query);
+		*query = NULL;
+	}
+	return status;
+}
+
+/**
+ * Adds a counter, specified by the specified counter path elements, 
+ * to the already opened query, specifying the handle that will be used
+ * to access the counter query results.
+ * Returns ERROR_SUCCESS on success or other PDH error code on failure.
+ * wchar_t *object performance counter object (eg Processor)
+ * wchar_t *instance counter instance (eg _Total) use 
+ * L"*" for a wild card.
+ * wchar_t *counterName English name of counter (eg % Processor Time).
+ * PDH_HQUERY *query handle to the query that will be used in subsequent
+ * calls (eg adding additional counters, running the query - PdhCollectQueryData). 
+ * Set to NULL if initialising the query fails.
+ * PDH_HCOUNTER *counter handle to the counter that will contain the
+ * counter query results and accessed in subsequent calls (eg getRawCounterValue,
+ * PdhGetFormattedCounterValue).
+ */
+PDH_STATUS addCounterToQuery(wchar_t *object, wchar_t *instance, wchar_t *counterName,
+							 PDH_HQUERY *query, PDH_HCOUNTER *counter)
+{
+	LPWSTR counterPath = getCounterPath(object, instance, counterName);
+	PDH_STATUS status = PdhAddEnglishCounterW(*query, counterPath, 0, counter);
+	my_free(counterPath);
+	return status;
+}
+
+/**
+ * Returns the raw counter value accessed from the specified counter
+ * handle. Returns 0 if there is an error on accessing the counter value.
+ * PDH_HCOUNTER *counter handle to counter that contains the counter
+ * value.
+ */
+LONGLONG getRawCounterValue(PDH_HCOUNTER *counter)
+{
+	DWORD dwType;
+	PDH_RAW_COUNTER value;
+	if (PdhGetRawCounterValue(*counter, &dwType, &value) == ERROR_SUCCESS) {
+		return value.FirstValue;
+	}
+	return 0;
+}
+
+/**
+ * Allocated sufficient memory for a number of raw counter items, populates 
+ * these counter items with the values of the counters countained in counters. 
+ * Returns the number of counters in the structure,
+ * excluding the _Total instance. This is for use when the counter query has
+ * been constructed using a wild card instance to collect counter values
+ * for all instances.
+ * If getting the counter values fails for some reason any memory allocated is
+ * freed, in all other cases memory for the counter items should be freed using 
+ * my_free when no longer needed.
+ * PDH_HCOUNTER *counter handle to counter that contains the counter
+ * values.
+ * PPDH_RAW_COUNTER_ITEM_W *values will be assigned to structure allocated by
+ * this function to contain the counter values from the counter.
+ */
+uint32_t getRawCounterValues(PDH_HCOUNTER *counter, PPDH_RAW_COUNTER_ITEM_W *values)
+{
+	DWORD bufSize = 0;
+	DWORD itemCount = 0;
+	uint32_t ret = 0;
+	if (PdhGetRawCounterArrayW(*counter, &bufSize, &itemCount, NULL) == PDH_MORE_DATA) {
+		*values = (PPDH_RAW_COUNTER_ITEM_W)my_calloc(bufSize);
+		if (PdhGetRawCounterArrayW(*counter, &bufSize, &itemCount, *values) == ERROR_SUCCESS) {
+			ret = itemCount;
+			if (ret > 0 && wcscmp(COUNTER_INSTANCE_TOTAL, values[0][itemCount-1].szName) == 0) {
+				ret--; // use readSingleCounter if you need _Total;
+			}
+		} else {
+			my_free(*values);
+		}
+	}
+	return ret;
+}
+	
+/**
+ * Returns the formatted (cooked), LONGLONG counter value obtained from the specified
+ * counter, using PdhGetFormattedCounterValue.
+ * If the counter cooking requires 2 counter readings and the counter only references
+ * one, returns 0.
+ * Returns 0 if the counter cooking fails for any other reason.
+ */
+LONGLONG getCookedCounterValue(PDH_HCOUNTER *counter)
+{
+	DWORD dwType;
+	PDH_FMT_COUNTERVALUE value;
+	if (ERROR_SUCCESS == PdhGetFormattedCounterValue(*counter, PDH_FMT_LARGE, &dwType, &value)) {
+		return value.largeValue;
+	}
+	return 0;
+}
+
+#if defined(__cplusplus)
+} /* extern "C" */
+#endif
