047849e6cb4a5fe8af71787d0eb2ab9d19abb858
neilmckee <neilmckee@c0bd136a-9412-4574-afac-2923e5188ea6>
2015-03-06 08:46:45 +0000

    tagging release 1_27_2

---
+++ tags/REL-1_27_2/src/Linux/hsflow_ethtool.h
@@ -0,0 +1,47 @@
+/* This software is distributed under the following license:
+ * http://host-sflow.sourceforge.net/license.html
+ */
+
+#ifndef HSFLOW_ETHTOOL_H
+#define HSFLOW_ETHTOOL_H 1
+
+#if defined(__cplusplus)
+extern "C" {
+#endif
+
+#ifdef HSP_ETHTOOL_STATS
+  static const char *HSP_ethtool_mcasts_in_names[] = {
+    "HwIfInMcastPkts",
+    "receive-multicast-packet",
+    NULL
+  };
+  static const char *HSP_ethtool_mcasts_out_names[] = {
+    "HwIfOutMcastPkts",
+    "tx-multicast-packets",
+    NULL
+  };
+  static const char *HSP_ethtool_bcasts_in_names[] = {
+    "HwIfInBcastPkts",
+    "receive-broadcast-packet",
+    NULL
+  };
+  static const char *HSP_ethtool_bcasts_out_names[] = {
+    "HwIfOutBcastPkts",
+    "tx-broadcast-packets",
+    NULL
+  };
+#endif
+
+#ifdef HSF_DOCKER
+  static const char *HSP_ethtool_peer_ifindex_names[] = {
+    "peer_ifindex",
+    NULL
+  };
+#endif
+
+#if defined(__cplusplus)
+} /* extern "C" */
+#endif
+
+#endif /* HSFLOW_ETHTOOL_H */
+
---
+++ tags/REL-1_27_2/src/Linux/readTcpipCounters.c
@@ -0,0 +1,83 @@
+/* This software is distributed under the following license:
+ * http://host-sflow.sourceforge.net/license.html
+ */
+
+#if defined(__cplusplus)
+extern "C" {
+#endif
+
+#include "hsflowd.h"
+
+  // make sure we read whole lines at a time
+#define MAX_PROC_LINE_CHARS 2048
+
+  /*_________________---------------------------__________________
+    _________________    parseCounterArray      __________________
+    -----------------___________________________------------------
+  */
+
+  static int parseCounterArray(char *str, uint32_t *counters, int n) {
+    char *p = str;
+    int ff = 0;
+    for(; ff < n; ff++) {
+      char buf[MAX_PROC_LINE_CHARS];
+      char *var = parseNextTok(&p, " \t", NO, 0, NO, buf, MAX_PROC_LINE_CHARS);
+      // stop if we reach the end of the line - or if something was not a number
+      if(var == NULL) {
+	// no more tokens
+	break;
+      }
+      char *end = NULL;
+      long val = strtol(var, &end, 0);
+      if(end == var) {
+	// nothing was consumed - it wasn't a number
+	break;
+      }
+      counters[ff] = (uint32_t)val;
+    }
+    return ff;
+  }
+
+  /*_________________---------------------------__________________
+    _________________    readTcpipCounters      __________________
+    -----------------___________________________------------------
+  */
+  
+  int readTcpipCounters(HSP *sp, SFLHost_ip_counters *c_ip, SFLHost_icmp_counters *c_icmp, SFLHost_tcp_counters *c_tcp, SFLHost_udp_counters *c_udp) {
+    
+#ifdef HSF_CUMULUS
+    return NO;
+#else
+    int count = 0;
+    FILE *procFile;
+    char line[MAX_PROC_LINE_CHARS];
+    
+    procFile= fopen("/proc/net/snmp", "r");
+    if(procFile) {
+      while(fgets(line, MAX_PROC_LINE_CHARS, procFile)) {
+	char *p = line;
+	char buf[MAX_PROC_LINE_CHARS];
+	char *var = parseNextTok(&p, " \t", NO, 0, NO, buf, MAX_PROC_LINE_CHARS);
+	if(strcmp(var, "Ip:") == 0) {
+	  count += parseCounterArray(p, (uint32_t *)c_ip, SFLHOST_NUM_IP_COUNTERS);
+	}
+	else if(strcmp(var, "Icmp:") == 0) {
+	  count += parseCounterArray(p, (uint32_t *)c_icmp, SFLHOST_NUM_ICMP_COUNTERS);
+	}
+	else if(strcmp(var, "Tcp:") == 0) {
+	  count += parseCounterArray(p, (uint32_t *)c_tcp, SFLHOST_NUM_TCP_COUNTERS);
+	}
+	else if(strcmp(var, "Udp:") == 0) {
+	  count += parseCounterArray(p, (uint32_t *)c_udp, SFLHOST_NUM_UDP_COUNTERS);
+	}
+      }
+      fclose(procFile);
+    }
+    return (count > 0);
+#endif
+  }
+
+#if defined(__cplusplus)
+} /* extern "C" */
+#endif
+
