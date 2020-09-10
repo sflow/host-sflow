da5fa6e708b25210ccb568f1e2e2a71e62d65607
Neil McKee <neil.mckee@inmon.com>
2020-09-10 13:08:22 -0700

    use pcap immediate-mode with kernel sampling

--- src/Linux/mod_pcap.c
+++ src/Linux/mod_pcap.c
@@ -256,20 +256,54 @@ extern "C" {
     
     bpfs->samplingRate = lookupPacketSamplingRate(bpfs->adaptor, sp->sFlowSettings);
     bpfs->subSamplingRate = bpfs->samplingRate;
-    bpfs->pcap = pcap_open_live(bpfs->deviceName,
-				sp->sFlowSettings_file->headerBytes,
-				bpfs->promisc,
-				0, /* timeout==poll */
-				bpfs->pcap_err);
-    if(bpfs->pcap == NULL) {
+
+    // create pcap
+    if((bpfs->pcap = pcap_create(bpfs->deviceName, bpfs->pcap_err)) == NULL) {
       myLog(LOG_ERR, "PCAP: device %s open failed: %s", bpfs->deviceName, bpfs->pcap_err);
       return;
     }
+
+    // immediate mode
+    if(kernelVer64(sp) < 3019000L) {
+      myDebug(1, "PCAP: kernel too old for BPF sampling, so not setting immediate mode");
+    }
+    else if(pcap_set_immediate_mode(bpfs->pcap, YES) != 0) {
+      myLog(LOG_ERR, "PCAP: pcap_set_immediate_mode(%s) failed", bpfs->deviceName);
+    }
+
+    // snaplen
+    if(pcap_set_snaplen(bpfs->pcap, sp->sFlowSettings_file->headerBytes) != 0)
+      myLog(LOG_ERR, "PCAP: pcap_set_snaplen(%s) failed", bpfs->deviceName);
+
+    // promiscuous mode
+    if(pcap_set_promisc(bpfs->pcap, bpfs->promisc) != 0)
+      myLog(LOG_ERR, "PCAP: pcap_set_promisc(%s) failed", bpfs->deviceName);
+
+    // read timeout
+    if(pcap_set_timeout(bpfs->pcap, 0) != 0)    // indicate we are going to poll
+      myLog(LOG_ERR, "PCAP: pcap_set_timeout(%s) failed", bpfs->deviceName);
+
+    // activate
+    int status = pcap_activate(bpfs->pcap);
+    if(status < 0) {
+      myLog(LOG_ERR, "PCAP: activate(%s) ERROR: %s", bpfs->deviceName, pcap_geterr(bpfs->pcap));
+      return;
+    }
+    else if(status > 0) {
+      myLog(LOG_INFO, "PCAP: activate(%s) warning: %s", bpfs->deviceName, pcap_geterr(bpfs->pcap));
+    }
     
     myDebug(1, "PCAP: device %s opened OK", bpfs->deviceName);
+
+    // get file descriptor
     int fd = pcap_fileno(bpfs->pcap);
+
+    // configure BPF sampling
     setKernelSampling(sp, bpfs, fd);
+
+    // register
     bpfs->sock = EVBusAddSocket(mod, mdata->packetBus, fd, readPackets_pcap, bpfs);
+
     // assume we always want to get counters for anything we are tapping.
     // Have to force this here in case there are no samples that would
     // trigger it in readPackets.c:takeSample()
